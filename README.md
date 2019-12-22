d00a12691b43e0ff9c02539a424ffe46306faf99
tbarnhill <58178655+tbarnhill@users.noreply.github.com>
2019-12-22 13:57:39 -0600

    Alpha Code

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # modbus-tcp
 A simple interface for Modbus over TCP/IP
+
+####$ Applicable Datatypes
+Data Type                  Short Hand   Size        Accessibility     
+Descrite Input             i_b          1 Bit       Read Only
+Coil                       q_b          1 Bit       Read / Write
+Input Register             i_w          16 Bits     Read Only
+Holding Register           q_w          16 Bits     Read / Write
+
+####$ Exports
+    ipAddress  - Ip Address requests will be sent to
+    port - Port requests will be sent to. Usually 502
+    log -  Can be set to 'True' to log all trafic on the console
+    client - Inhearits from the 'Net' module 
+
+    read([address],[callback])
+    write([address],[value],[callback])
+
+#####$ Read and Write
+``` javascript
+const modbus = require('modbus-tcp-ip')
+
+//Multiple Devises can be added
+const deviceA = new modbus('192.168.0.1',502)
+const deviceB = new modbus('192.168.0.2',502)
+
+//Read descrite input at address 1
+deviceA.read('i_b1',(err,res)=>{
+        if(err){throw err}
+        console.log(res)
+})
+
+//Write Coil at address 1 
+deviceA.write('q_b1',true,(err,res)=>{
+        if(err){throw err}
+        console.log(res)
+})
+```
+
+###### If you wish to handle the TCP yourself 
+``` javascript
+
+let transId     = 0
+let protoId     = 0
+let unitId      = 1
+let funcCode    = 5
+let address     = 0
+let data        = 0xFF00
+let dataPacket = device.makeDataPacket(transId,protoId,unitId,funcCode,address,data)
+
+let net = require('net')
+let client = new net.Socket()
+client.setEncoding('utf8')
+
+client.connect(502, '192.168.0.001', ()=> {
+          client.write(dataPacket)
+})
+
+client.on('data',(res,err)=>{
+        let hex = Buffer.from(res).toString('hex') //Converts response to hexadecimal string
+        console.log('rx '+hex)
+})
+```
---
+++ script.js
@@ -0,0 +1,152 @@
+/**
+    *Discrete Input     i_b       1 Bit     Read Only
+    *Coil               q_b       1 Bit     Read / Write
+    *Input Register     i_w      16 Bits    Read Only
+    *Holding Register   q_w      16 Bits    Read / Write
+ */
+
+module.exports = new ModbusTcp()
+
+function ModbusTcp(ipAddress,port){
+    let net = require('net')
+    this.client = new net.Socket()
+    this.client.setEncoding('utf8')
+    this.ipAddress = ipAddress
+    this.port = port
+    this.log = true
+
+    this.sendTCP=(data,callback)=>{
+        if(this.log==true){console.log('tx '+data).toString('hex')}
+        let buffer = Buffer.from(data, "hex")
+        if(this.isOpen()){this.client.write(buffer)}
+        else{this.connect(()=>{this.client.write(buffer)})}
+
+        
+        
+         this.client.on('data',(res,err)=>{
+                let hex = Buffer.from(res).toString('hex') //convert response to hexadecimal 
+                if(this.log==true){console.log('rx '+hex)}
+                if(callback){callback(err,res)}  
+         })
+         this.client.on('close',(err)=>{   
+                err = 'Connection Closed'
+                if(callback){callback(err,null)}
+         })
+    }
+    let getFuncCode=(address,readOrWrite)=>{
+
+        let functionCode
+        if(readOrWrite=='read'){
+            if(dataType=='i_b'){functionCode=2}
+            if(dataType=='q_b'){functionCode=1}
+            if(dataType=='i_w'){functionCode=4}
+            if(dataType=='q_w'){functionCode=3}
+        }
+        else if(readOrWrite=='write'){
+            if(dataType=='i_w'||dataType=='i_b'){throw 'you cannot write to a read only register'}
+            if(dataType=='q_w'){functionCode=6}
+            if(dataType=='q_b'){functionCode=5}
+        }
+        else{
+            throw "parameter 1 must be 'read' or 'write'"
+        }
+
+     
+        return functionCode
+      
+    
+    }
+
+    this.read = (address,callback)=>{
+        let funcCode = getFuncCode(address,readOrWrite)
+        address = address.substr(3)
+        let hexString = this.makeDataPacket(1,0,1,funcCode,address,0)
+
+        this.sendTCP(hexString,0,(err,res)=>{
+            if(callback){callback(err,res)}
+        })
+    }
+    this.write = (address,value,callback)=>{
+        let funcCode = getFuncCode(address,readOrWrite)
+        address = address.substr(3)
+
+        if (funcCode==5&&value==true){value = 65280} // To force a coil on you send FF00 not 0001
+
+        let hexString = this.makeDataPacket(1,0,1,funcCode,address,value)
+
+        this.sendTCP(hexString,(err,res)=>{
+            if(err){throw err}
+            if(callback){callback(err,res)}
+        })
+    }
+    this.send=(functionCode,address,data,callback)=>{
+        
+
+        let hexString = this.makeDataPacket(1,0,1,functionCode,address,data)
+        
+        if(this.log==true){console.log('tx '+hexString)}
+        sendTCP(hexString,(res)=>{
+            let hex = Buffer.from(res).toString('hex') //convert response to hexadecimal 
+            if(this.log==true){console.log('rx '+hex)}
+            if(callback){callback()}
+        })
+       
+
+       
+    }
+    this.connect=(callback)=>{
+        this.client.connect(this.port, this.ipAddress, (err)=> {
+            if(err){throw err}
+            if(callback){callback()}
+        })
+    }
+    this.isOpen=()=>{
+        return this.client.writable
+    }
+    this.makeDataPacket=(transId,protoId,unitId,funcCode,address,data)=>{
+        address = address -1
+    
+        unitId = decToHex(unitId)
+        transId = decToHex(transId)
+        protoId = decToHex(protoId)
+        funcCode = decToHex(funcCode)
+        address = decToHex(address)
+        data = decToHex(data)
+    
+        unitId = makeHexConstantSize(unitId,2)
+        transId = makeHexConstantSize(transId,4)
+        protoId = makeHexConstantSize(protoId,4)
+        funcCode = makeHexConstantSize(funcCode,2)
+        address = makeHexConstantSize(address,4)
+        data = makeHexConstantSize(data,4)
+    
+        let length = '0006'
+    
+        function makeHexConstantSize(data,outputCharCount){
+            //Note 2 hex chars = 1 byte
+            let inputCharCount = data.length
+            let charsToAdd = outputCharCount - inputCharCount
+            if(charsToAdd<0){
+                throw 'input hex char count is greater then the output'
+            }
+          
+            let adder = ''
+            for(let i=0;i<charsToAdd;i++){adder+='0'}
+            return adder+data
+        }
+        function decToHex(c){
+            let rtn 
+           
+            rtn = Math.abs(c).toString(16)
+           
+    
+            return rtn
+        }
+        
+    
+        return transId + protoId + length + unitId + funcCode + address + data
+    
+    }
+
+}
+
