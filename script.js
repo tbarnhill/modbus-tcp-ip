edc2ef37e31ff879e251df6ce833de77d1d72277
tbarnhill <58178655+tbarnhill@users.noreply.github.com>
2020-01-17 13:36:34 -0600

    Holding registers now work

--- script.js
+++ script.js
@@ -13,45 +13,98 @@ module.exports = {
 function ModbusTcp(ipAddress,port,unitId){
     let net = require('net')
     this.client = new net.Socket()
-    this.client.setEncoding('utf8')
+    //this.client = net.createConnection(port,ipAddress,()=>{})
+    this.client.setEncoding('hex')
     this.ipAddress = ipAddress
     this.port = port
     this.unitId = unitId
     this.log = false
+    let lastTid = 1
+    this.online = false
+    this.timeout = 10000
+    this.packetBufferLength = 100
+    this.packets = []
 
-    let tcpCallback //allows callback to be set in sendTCP and called in 'On Data' event
+
     this.sendTCP=(data,callback)=>{
         if(this.log==true){console.log('tx '+data)}
         let buffer = Buffer.from(data, "hex")
-        if(this.isOpen()){this.client.write(buffer)}
+        if(this.client.writable){this.client.write(buffer)}
         else{this.connect(()=>{this.client.write(buffer)})}
-        if(callback){tcpCallback=callback}else{tcpCallback=null}
+
+
+        //let tid = parseInt(data.substr(0,4),16)
+        
+
+        
     }
-    this.client.on('data',(res,err)=>{
-        let buf = Buffer.from(res)
-        if(this.log==true){console.log('rx '+ buf.toString('hex') )}
-        if(tcpCallback){tcpCallback(err,buf)}  
-        this.client.destroy()
+    this.client.on('data',(res)=>{
+
+        if(this.log==true){console.log('rx '+ res)}
+
+        let modbusRes = parseResponse(res)
+        let value = modbusRes.value
+        let tid = modbusRes.tid
+    
+      
+        let err = null
+        this.packets[tid].rx = modbusRes
+        this.packets[tid].rx.hex = res
+        if(this.packets[tid].onResponce){
+            this.packets[tid].onResponce(err,value)
+        } 
+        
+        //this.client.destroy()
+    })
+    this.client.on('close',()=>{
+        this.online=false
+    })
+    this.client.on('connect',()=>{
+        this.online=true
     })
     this.read = (address,callback)=>{
         let funcCode = getFuncCode(address,'read')
+        let tid = getTid()
+
         address = address.substr(3)
-        let hexString = makeDataPacket(1,0,1,funcCode,address,1)
-        this.sendTCP(hexString,(err,res)=>{
-            if(callback){callback(err,res[res.length-1])}
-        })
+
+        let hexString = makeDataPacket(tid,0,1,funcCode,address,1)
+
+        let packet = {}
+        packet.onResponce = callback
+        packet.tx = {}
+        packet.tx.funcCode = funcCode
+        packet.tx.tid = tid
+        packet.tx.address = address
+        packet.tx.hex = hexString
+        packet.rx = null
+        this.packets[tid] = packet
+
+        
+        this.sendTCP(hexString,callback)
     }
     this.write = (address,value,callback)=>{
         let funcCode = getFuncCode(address,'write')
+        let tid = getTid()
         address = address.substr(3)
-    
+
         if (funcCode==5&&value==true){value = 65280} // To force a coil on you send FF00 not 0001
-    
-        let hexString = makeDataPacket(1,0,this.unitId,funcCode,address,value)
-    
-        this.sendTCP(hexString,(err,res)=>{
-            if(callback){callback(err,res)}
-        })
+
+        let hexString = makeDataPacket(tid,0,this.unitId,funcCode,address,value)
+
+        let packet = {}
+        packet.onResponce = callback
+        packet.tx = {}
+        packet.tx.funcCode = funcCode
+        packet.tx.tid = tid
+        packet.tx.address = address
+        packet.tx.hex = hexString
+        packet.rx = null
+        this.packets[tid] = packet
+      
+        
+  
+        this.sendTCP(hexString,callback)
     }
     this.connect=(callback)=>{
         
@@ -60,9 +113,7 @@ function ModbusTcp(ipAddress,port,unitId){
         })
 
     }
-    this.isOpen=()=>{
-        return this.client.writable
-    }
+    
     function getFuncCode(address,readOrWrite){
         let dataType = address.substr(0,3)
     
@@ -87,6 +138,12 @@ function ModbusTcp(ipAddress,port,unitId){
       
     
     }
+    
+    let getTid=()=>{
+        if(this.lastTid==this.packetBufferLength){this.lastTid=0}
+        return lastTid++
+    }
+   
 }
 
 function makeDataPacket(transId,protoId,unitId,funcCode,address,data){
@@ -135,4 +192,41 @@ function makeDataPacket(transId,protoId,unitId,funcCode,address,data){
 
     return transId + protoId + length + unitId + funcCode + address + data
 
+}
+function parseResponse(buf){
+    buf = buf.toString()
+    let tid = buf.substr(0,4)
+    let protocallId = buf.substr(4,4)
+    let length = buf.substr(8,4)
+    length = parseInt(length,16)
+
+    let unitId = buf.substr(12,2)
+    let funcCode = buf.substr(14,2)
+    let byteCount = buf.substr(16,2)
+    byteCount = parseInt(byteCount,16)
+    let value = buf.substr(12+(length*2)-(byteCount*2),byteCount*2)
+
+
+  
+
+    tid = parseInt(tid,16)
+    protocallId = parseInt(protocallId,16)
+   
+    unitId = parseInt(unitId,16)
+    funcCode = parseInt(funcCode,16)
+    
+    value = parseInt(value,16)
+
+
+    let rtn =  {
+        tid:tid,
+        protocallId:protocallId,
+        length:length,
+        unitId:unitId,
+        funcCode:funcCode,
+        byteCount:byteCount,
+        value:value
+    }
+
+    return rtn
 }
